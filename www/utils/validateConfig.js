f34699a0f92e37fc45ec24dffb19b5b827b43edc
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2023-03-01 15:12:58 -0300

    #749680 - adding navSettings param (#16)

---
+++ www/utils/validateConfig.js
@@ -0,0 +1,5 @@
+function isEmpty(config) {
+  return Object.keys(config).length !== 0;
+}
+
+module.exports = isEmpty;
--- www/wazenavigator.js
+++ www/wazenavigator.js
@@ -1,30 +1,36 @@
+const configIsEmpty = require('./utils/validateConfig');
+
 /*
  * Created by adaltojunior on 6/11/15.
  */
 WazeNavigator = {
-    navigateByPosition: function (from, to) {
-
-        try{
-            if ((!isNaN(from.lat) && !isNaN(from.lng)) &&
-                (!isNaN(to.lat) && !isNaN(to.lng))) {
-                cordova.exec(
-                    function () {
-                        console.log('Good navigation!!!');
-                    },
-                    function () {
-                        console.log('Sorry, there are problems to navigate!!!');
-                    },
-                    'WazeNavigator',
-                    'navigateByWaze',
-                    [from.lat, from.lng, to.lat, to.lng]);
-            } else {
-                console.log("Latitude and longitude aren't numbers.");
-            }
-        } catch (e) {
-            console.log("Error on navigate by position: " + e.message);
-        }
-
+  navigateByPosition: function (from, to, navSettings) {
+    try {
+      if (
+        !isNaN(from.lat) &&
+        !isNaN(from.lng) &&
+        !isNaN(to.lat) &&
+        !isNaN(to.lng) &&
+        !configIsEmpty(navSettings)
+      ) {
+        cordova.exec(
+          function () {
+            console.log("Good navigation!!!");
+          },
+          function () {
+            console.log("Sorry, there are problems to navigate!!!");
+          },
+          "WazeNavigator",
+          "navigateByWaze",
+          [from.lat, from.lng, to.lat, to.lng, navSettings]
+        );
+      } else {
+        console.log("Latitude and longitude aren't numbers.");
+      }
+    } catch (e) {
+      console.log("Error on navigate by position: " + e.message);
     }
-
+  },
 };
+
 module.exports = WazeNavigator;
